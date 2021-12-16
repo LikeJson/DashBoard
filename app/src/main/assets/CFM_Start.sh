399565c3d1f9ef87fce24ded2576d7f921801c24
Maruko <zhxuhu@outlook.com>
2021-12-17 07:41:35 +0800

    Update CFM_Start.sh

--- app/src/main/assets/CFM_Start.sh
+++ app/src/main/assets/CFM_Start.sh
@@ -5,5 +5,4 @@ else
   MAGISK_PATH="/data/adb/modules"
 fi
 
-disABpath=${MAGISK_PATH}/Clash_For_Magisk/disable
-rm -f ${disABpath}
+/data/adb/modules/Clash_For_Magisk/scripts/clash.service -s && /data/adb/modules/Clash_For_Magisk/scripts/clash.tproxy -
