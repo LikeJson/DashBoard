0962fb845b989d31d0e8ad69be229687acf200f9
admin <2833154405@qq.com>
2022-01-23 12:35:16 +0800

    make libchecker happy?

--- app/src/main/java/com/dashboard/kotlin/WebViewPage.kt
+++ app/src/main/java/com/dashboard/kotlin/WebViewPage.kt
@@ -9,7 +9,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebSettings
-import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.annotation.RequiresApi
 import androidx.core.content.res.ResourcesCompat
